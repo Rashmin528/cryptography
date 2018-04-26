5353e3a50b1313ba8544a52e65124029e069b042
Rashmin M <37808666+Rashmin528@users.noreply.github.com>
2018-04-27 01:03:07 +0530

    decryption

---
+++ decrypt.py
@@ -0,0 +1,36 @@
+import pyperclip
+ 
+plaintext = 'PNRFNEPVCURE.'
+ 
+key = 13
+ 
+mode = 'decrypt' 
+ 
+letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+ 
+translated = ''
+
+plaintext = plaintext.upper()
+ 
+for symbol in plaintext:
+	if symbol in letters:
+		num = letters.find(symbol) 
+		if mode == 'encrypt':
+			num = num + key
+		elif mode == 'decrypt':
+			num = num - key
+
+		if num >= len(letters):
+			num = num - len(letters)
+		elif num < 0:
+			num = num + len(letters)
+		translated = translated + letters[num]
+
+else:
+		translated = translated + symbol
+ 
+print(translated)
+ 
+pyperclip.copy(translated)
+
+#python3 decrypt.py
